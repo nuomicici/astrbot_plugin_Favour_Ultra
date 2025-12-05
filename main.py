9371d7fbf482e01e1a9d93d897efe39f84a2bdeb
糯米茨 <143102889+nuomicici@users.noreply.github.com>
2025-12-05 15:56:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ from .utils import is_valid_userid
 from .permissions import PermLevel, PermissionManager
 from .storage import FavourFileManager, GlobalFavourFileManager
 
-@register("favour_manager", "AstrBot", "好感度管理插件", "1.1.0")
 class FavourManagerTool(Star):
     DEFAULT_CONFIG = {
         "min_favour_value": -100,
